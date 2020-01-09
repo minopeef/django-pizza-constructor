7536d0f543408ef7f7c47e81a20c604fc0adb4d8
Oleksandr <oleksandrko448@gmail.com>
2020-01-09 15:01:36 +0200

    fix styles

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+npho^jq7hds$fzok6bqdb2w^o4$ypu8r@qr+$!gr3b!fiemmc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
