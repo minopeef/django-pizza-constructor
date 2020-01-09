fc663bb3a8e2f442aef258fc797f4d7ad28a7e91
Oleksandr <oleksandrko448@gmail.com>
2020-01-09 13:56:20 +0200

    update settings

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'pizza_app.apps.PizzaAppConfig',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
