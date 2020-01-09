793de3290621160783928acd546c45d367919996
Oleksandr <oleksandrko448@gmail.com>
2020-01-09 19:34:30 +0200

    use env variables

--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -133,9 +133,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get("EMAIL_PASSWORD")
-# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_HOST_USER = "django.pizza.constructor@gmail.com"
-EMAIL_HOST_PASSWORD = "pizzaapp123"
+EMAIL_HOST_USER = os.environ.get("EMAIL_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST_USER = "your google email"
+# EMAIL_HOST_PASSWORD = "your password"
 
 django_heroku.settings(locals())
