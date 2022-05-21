d05874bcf8e643d39301a78a0a7e5e4a4d4f3ef1
Oleksandr <oleksandrko448@gmail.com>
2022-05-21 11:05:09 +0300

    Update admin.py

--- pizza_app/admin.py
+++ pizza_app/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import ToppingGroup, Topping, OptionGroup, Option, Order, OrderOption, OrderTopping
 
-admin.site.register([ToppingGroup, Topping, OptionGroup,
-                     Option,  Order, OrderOption, OrderTopping])
+admin.site.register([ToppingGroup, Topping, OptionGroup, Option, Order, OrderOption, OrderTopping])
